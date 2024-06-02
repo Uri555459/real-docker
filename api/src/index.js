b50758fe617268c914148500f6f855afd1c31d0e
Uri <uri555459@gmail.com>
2024-06-02 13:01:12 +0300

    Added environments

--- api/src/index.js
+++ api/src/index.js
@@ -2,8 +2,14 @@ const express = require('express')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+const host = process.env.HOST
+
 app.get('/test', (req, res) => {
 	res.send('Сервер запущен и работает.')
 })
 
-app.listen(3000, console.log('Сервер API запущен'))
+app.listen(port, () => {
+	console.log(`Сервер API запущен на порту ${port}`)
+	console.log(`Хост ${host}`)
+})
